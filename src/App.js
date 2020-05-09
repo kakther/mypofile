fb217e1f939865a84c732df687f5494de5abe425
Kausar Akther <kausar.akther@gmail.com>
2020-05-08 23:44:06 -0400

    Added Nav * Funcs

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/main';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+// import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title="Title" scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link> #Add updated resume
-                <Link to="/aboutme">About ME</Link>
-                <Link to="/project">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <Link to="/resume">Resume</Link> #Add updated resume
-                <Link to="/aboutme">About ME</Link>
-                <Link to="/project">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
-      
-    </div>
-  );
+// Components 
+import Nav from './components/Nav';
+// import Home from './pages/Home';
+// import Footer from './components/Footer';
+// import Villa from './pages/Villa';
+// import contact from './components/contact';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <>
+          <Router>
+            <div>
+              <Nav />
+              <Switch>
+                {/* <Route path='/' exact component={Home} /> */}
+                {/* <Route path='/Resume' component={Resume} /> */}
+              </Switch>
+            </div>
+          </Router>
+          {/* <Footer /> */}
+        </>
+      </div>
+    )
+  }
 }
 
 export default App;
